492db5367bad38d13a56f7b4bf3fb8c870485e0d
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-02-06 17:44:48 +0700

    dem so lan xuat hien cua ky tu trong chuoi

---
+++ NgonNguCSharp_BaiTapFunctionMethod_Dem1KyTuTrongChuoi/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgonNguCSharp_BaiTapFunctionMethod_Dem1KyTuTrongChuoi
+{
+    internal class Program
+    {
+        public static int DemKyTu(string str, char item)
+        {
+            int count = 0;
+            str = str.Replace(" ", string.Empty);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char str_item = (char)str[i];
+                if (str_item == item)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            Console.Write("Nhap vao chuoi: ");
+            string str = Console.ReadLine();
+
+            //Xóa khoảng trắng khỏi chuỗi
+            str = str.Replace(" ", string.Empty);
+
+            Console.Write("Nhap vao ky tu can dem: ");
+            char item = char.Parse(Console.ReadLine());
+
+            count = DemKyTu(str, item);
+
+            if (count == 0)
+            {
+                Console.WriteLine("Khong ton tai ky tu " + item + " trong chuoi tren");
+            }
+            else
+            {
+                Console.WriteLine("Co tat ca " + count + " ky tu " + item + " trong chuoi tren");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
